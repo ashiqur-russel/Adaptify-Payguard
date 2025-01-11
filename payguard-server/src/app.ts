d64364655ec0758c229375209a749c7ccbdca811
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-01-11 19:55:00 +0100

    Backend with express api initialized

---
+++ payguard-server/src/app.ts
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    message: 'Server is up and running!',
+  });
+});
+
+export default app;
---
+++ payguard-server/src/app/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_secret_expires: process.env.JWT_ACCESS_EXPIRATION,
+  environment: process.env.NODE_ENV,
+  bcrypt_salt_rounds: process.env.SALT,
+  supabase_project_url: process.env.SUPABASE_PROJECT_URL,
+  supabase_api_key: process.env.SUPABASE_API_KEY,
+  stripe_secret_key: process.env.STRIPE_SECRET_KEY,
+};
